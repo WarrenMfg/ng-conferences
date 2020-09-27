bec804615998cfcaa3f0f5302aaae5938deb174c
WarrenMfg <warrenmfg@icloud.com>
2020-09-27 12:29:48 -0400

    adds navbar

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NavBarComponent } from './nav/navbar.component';
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    NavBarComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
-export class EventsAppModule { }
+export class EventsAppModule {}
